beb0527883b3ecfdfa1d7606fad61e79fa0786fa
Coopah <matheussenjasper@icloud.com>
2022-12-14 12:01:24 +0100

    Added a constuctor for using the contentmanager in the child classes

--- gamedevGame/Characters/Character.cs
+++ gamedevGame/Characters/Character.cs
@@ -4,13 +4,21 @@ namespace gamedevGame;
 
 public class Character : IGameObject, IMovable
 {
+    protected Texture2D Texture;
+    
     public Vector2 Position { get; set; }
     public Vector2 Speed { get; set; }
     public Vector2 GravityPull { get; set; }
     public IIinputReader InputReader { get; set; }
     public Rectangle Hitbox { get; set; }
+
+    public int Health;
+
+    protected Character(ContentManager content)
+    {
+    }
     
-    
+
     public virtual void Update(GameTime gameTime)
     {
     }
