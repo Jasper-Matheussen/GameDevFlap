c8a4b03609024922a3174f6bd7f9338b09b273d8
Coopah <matheussenjasper@icloud.com>
2022-12-15 11:57:15 +0100

    Changed the respawn position

--- gamedevGame/CollisionEvents/FlyCollision.cs
+++ gamedevGame/CollisionEvents/FlyCollision.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using gamedevGame.Collision;
 using gamedevGame.CollisionEvents;
+using gamedevGame.interfaces;
+using gamedevGame.LevelDesign;
+
 namespace gamedevGame.CollisionEvents
 {
 	public class FlyCollision : CollideWithEvent
@@ -9,11 +12,15 @@ namespace gamedevGame.CollisionEvents
 
 		public override void Execute(Hero hero)
 		{
-            _newPosition = new Vector2(50, 150);
+            _newPosition = new Vector2(150, 200);
             hero.Position = _newPosition;
             hero.Health -= 1;
             Console.WriteLine(hero.Health);
         }
+
+		public FlyCollision(Block block) : base(block)
+		{
+		}
 	}
 }
 
